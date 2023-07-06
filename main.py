514a2da0ae68b03c5cbbce52dae45cee7c51a959
Knut Selstad <knut.selstad@gmail.com>
2023-07-06 10:42:31 +0200

    The branches are merged

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 # Her skal alle funksjonene kalles opp og kjøres
 
-from Addisjon import addisjon
+from Addisjon import addisjon, add_3_numbers
+from div_file import diviation
+
 
 print(addisjon(1, 2))
+print(diviation(1, 2))
+print(add_3_numbers(1, 2, 3))
