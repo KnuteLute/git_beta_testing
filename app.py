fd78b0b6bf0d3125102dc9d9145ec1b66156ef1c
Knut Selstad <knut.selstad@gmail.com>
2023-07-10 09:52:12 +0200

    d adada

--- app.py
+++ app.py
@@ -4,97 +4,85 @@ import dash_mantine_components as dmc
 import os
 import plotly.graph_objects as go
 import dash_bootstrap_components as dbc
-
+import urllib.parse
+import plotly.express as px 
+import pandas as pd
 
 app = Dash(__name__, suppress_callback_exceptions=True)
 
 
-@callback(
-    Output('url', 'pathname'),
-    Input('draw', 'n_clicks'),
-    Input('reset', 'n_clicks'),
-    prevent_initial_call=True
-)
-def update_graph(button1, button2):
-    
+data = {
+    'Category': ['Cat', 'Cat', 'Cat', 'Dog', 'Dog', 'Dog', 'Fish', 'Fish', 'Fish'],
+    'Status': ['Positive', 'Negative', 'Uncategorized', 'Positive', 'Negative', 'Uncategorized', 'Positive', 'Negative', 'Uncategorized'],
+    'Count': [7, 1, 2, 3, 5, 6, 10, 11, 12]
+}
 
-    triggered_id = dash.callback_context.triggered[0]['prop_id'].split('.')[0]
-    # print(triggered_id)
-    if triggered_id == 'reset':
-         return '/'
-    elif triggered_id == 'draw':
-         return '/page1'
-
-def get_link():
-    return dcc.Link('Home', href='/dedef')
-
+df = pd.DataFrame(data)
 
 def get_graph():
-    
-    x_data = [7, 1, 2]
-    y_data = [0, 1, 2]
-    z_data = [10, 11, 12]
-    categories = ['Positive', 'Negative', 'Uncategorized']
-    fig = go.Figure(data=[
-        go.Bar(name='Positive', x=x_data, y=categories, orientation='h', marker_color='green'),
-        go.Bar(name='Negative', x=y_data, y=categories, orientation='h', marker_color='red'),
-        go.Bar(name='Uncategorized', x=z_data, y=categories, orientation='h', marker_color='blue')
-    ])
+    cat_data = [7, 1, 2]
+    dog_data = [3, 5, 6]
+    fish_data = [10, 11, 12]
+    categories = ['cat', 'dog', 'fish']
+    fig = px.bar(df, x="Category", y="Count", color="Status", title="Long-Form Input")
 
     fig.update_layout(barmode='stack')
 
     return fig
-
 
 def reset_graph():
     return go.Figure()
 
 graph_figure = get_graph()
 
-
 home_layout = html.Div([
     html.H1('Page 1'),
-    dcc.Link('Home', href='/'),
+    dcc.Link('Go to Page 2', href='/page2'),
     html.Br(),
-    html.Button('Draw Graph', id='draw'),
-    html.Button('Reset Graph', id='reset'),
     dcc.Graph(
         id='graph',
         figure=graph_figure,
         clickData=None 
-        ),
-   
+        )
 ])
 
+@app.callback(
+    Output('url', 'pathname'),
+    [Input('graph', 'clickData')],
+    prevent_initial_call=True
+)
+def update_url(clickData):
+    if clickData:
+        info = clickData['points'][0]['label']
+        new_url = f"/page2?info={urllib.parse.quote_plus(info)}"
+        return new_url
+    else:
+        raise dash.exceptions.PreventUpdate
 
-
-new_layout = html.Div([
-    html.H1('Page 1'),
-    dcc.Link('Home', href='/'),
-    html.Br(),
-    
-    html.Div(id='link-to-home')
-])
+@app.callback(
+    Output('page-content', 'children'),
+    [Input('url', 'pathname'), Input('url', 'search')]
+)
+def display_page(pathname, search):
+    if pathname == '/page2':
+        if search:
+            info = urllib.parse.unquote_plus(search.lstrip('?info='))
+            new_layout = html.Div([
+                html.H1('Page 2'),
+                dcc.Link('Go back to Page 1', href='/page1'),
+                html.Br(),
+                html.P(f'You clicked on: {info}')
+            ])
+            return new_layout
+        else:
+            return html.P('No information provided.')
+    else:
+        return home_layout
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
     html.Div(id='page-content')
 ])
 
-
-@app.callback(Output('page-content', 'children'),
-              [Input('url', 'pathname')])
-def display_page(pathname):
-    if pathname == '/page1':
-        return new_layout
-    else:
-        return home_layout
-    
-
 if __name__ == "__main__":
     app.run_server(debug=True, port=8050)
-
-
-
-
-
